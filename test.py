d2b6e088a44ddbc0d9c4bc55ec186ae616941192
nileshverma054 <51699234+nileshverma054@users.noreply.github.com>
2019-07-29 21:01:25 +0530

    Window size defined
    Contribute here furtther

---
+++ test.py
@@ -0,0 +1,17 @@
+import turtle
+
+x=594
+y=841
+
+#set up window size
+turtle.setup(x,y)
+
+wn = turtle.Screen()
+wn.bgcolor("#fff")
+wn.title("Turtle")
+box = turtle.Turtle()
+
+#turtle.goto(-100,0)
+#box.fd(600)
+
+turtle.done()
